3c015ca868f9d3b5d7bd3e7366ea3949858d747a
qin <1635640334@qq.com>
2024-06-23 00:19:48 +0800

    Delete test2 directory

--- test2/Base64.h
+++
@@ -1,9 +0,0 @@
-#ifndef BASE64_H
-#define BASE64_H
-
-#include <stdlib.h>
-
-char* base64_encode(const unsigned char* data, size_t input_length, size_t* output_length);
-unsigned char* base64_decode(const char* data, size_t input_length, size_t* output_length);
-
-#endif // BASE64_H
